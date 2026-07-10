8b4123128d4c5ede5a220f740ec5de71c9c2bb86
okoth kongo <okothkongo@gmail.com>
2026-07-10 20:49:58 +0300

    maybe test 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AtEx.MixProject do
     [
       app: :at_ex,
       version: "0.20.23",
-      elixir: "~> 1.18",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
